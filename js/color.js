34dd2da49188c30b8fc2ae265abbf59f058d010e
caesar93 <druid0107@163.com>
2015-08-21 23:00:10 +0800

    Test RGB

---
+++ js/color.js
@@ -0,0 +1,30 @@
+//十六进制颜色值的正则表达式
+var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+
+/*
+var colorHex = "#34538b"
+var ColorRgb = sHex.colorRgb();
+*/
+/*16进制颜色转为RGB格式*/
+String.prototype.hex2Rgb = function(){
+	var sColor = this.toLowerCase();
+	if(sColor && reg.test(sColor)){
+		if(sColor.length === 4){
+			var sColorNew = "#";
+			for(var i=1; i<4; i+=1){
+				sColorNew += sColor.slice(i,i+1).concat(sColor.slice(i,i+1));	
+			}
+			sColor = sColorNew;
+		}
+		//处理六位的颜色值
+		var sColorChange = [];
+		for(var i=1; i<7; i+=2){
+			sColorChange.push(parseInt("0x"+sColor.slice(i,i+2)));	
+		}
+		return sColorChange.join(",");
+	}else{
+		return sColor;	
+	}
+};
+
+
